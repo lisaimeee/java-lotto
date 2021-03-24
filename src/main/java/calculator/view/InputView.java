7bd82d8b56c607343ae6390300d1a8525c768b6e
jess <jess.l@kakaocorp.com>
2021-03-24 23:40:45 +0900

    fix(InputView): \n 입력 처리 버그 픽스
    커맨드라인에서 "\n"를 입력한 것과 String 에서 "\n" 차이가 있다.
    그래서 커맨드라인 "\n"를 입력한 경우
    String 의 "\n" 로 변환한다.

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,12 +1,18 @@
 package calculator.view;
 
+import calculator.domain.StringAdditionExpression;
+
 import java.util.Scanner;
 
 public class InputView {
-    public String getInput(){
+    private static final String SUFFIX_PATTERN = "\\\\n";
+
+    public String getInput() {
         Scanner scanner = new Scanner(System.in);
-        if(scanner.hasNext()){
-            return scanner.nextLine();
+        if (scanner.hasNext()) {
+            String formatted = scanner.nextLine()
+                    .replaceAll(SUFFIX_PATTERN, StringAdditionExpression.CUSTOM_SUFFIX);
+            return formatted;
         }
         throw new IllegalStateException("입력을 받을 수 없습니다.");
     }
